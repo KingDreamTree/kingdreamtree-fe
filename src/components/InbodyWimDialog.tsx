304a7d12d03f7ef925a8fd82ccc32a6cf17d6f61
geuniey <geuniey@gmail.com>
2026-08-20 01:02:09 +0900

    fix: WIM 업로드 안내 문구 띄어쓰기 수정

--- src/components/InbodyWimDialog.tsx
+++ src/components/InbodyWimDialog.tsx
@@ -10,7 +10,7 @@ export function InbodyWimDialog({ open, onClose, onConnect }: InbodyWimDialogPro
     <section className="inbody-wim-modal" role="dialog" aria-modal="true" aria-labelledby="inbody-wim-title">
       <span className="inbody-wim-modal__icon"><img src={inbodyInfo} alt="" /></span>
       <h2 id="inbody-wim-title">WIM 회원이신가요?</h2>
-      <p>WIM 회원이시면 인바디 정보를 연동하여<br />업로드 할 수 있어요!</p>
+      <p>WIM 회원이시면 인바디 정보를 연동하여<br />업로드할 수 있어요!</p>
       <button type="button" onClick={onConnect}>마이데이터 연동하기</button>
     </section>
   </>
